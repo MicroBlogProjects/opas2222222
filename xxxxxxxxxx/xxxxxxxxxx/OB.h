5ac2984d5eee6f20d29095ea136eb39f93688b9e
opaser <1017370773@qq.com>
2015-07-09 20:20:43 +0800

    C

---
+++ xxxxxxxxxx/xxxxxxxxxx/OB.h
@@ -0,0 +1,13 @@
+//
+//  OB.h
+//  xxxxxxxxxx
+//
+//  Created by administrator on 15/7/9.
+//  Copyright (c) 2015年 administrator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OB : NSObject
+
+@end
---
+++ xxxxxxxxxx/xxxxxxxxxx/OB.m
@@ -0,0 +1,13 @@
+//
+//  OB.m
+//  xxxxxxxxxx
+//
+//  Created by administrator on 15/7/9.
+//  Copyright (c) 2015年 administrator. All rights reserved.
+//
+
+#import "OB.h"
+
+@implementation OB
+
+@end
